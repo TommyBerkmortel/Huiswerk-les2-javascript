e993f59d647b499e29413aa25923e6315ab5d601
TommyBerkmortel <B_tommy_@hotmail.com>
2022-03-22 20:22:25 +0100

    Huiswerk klaar

--- loops.js
+++ loops.js
@@ -6,9 +6,9 @@
 // Hoera!
 // ==========================================
 
-/*for (let i = 0; i < 3; i++) {
+for (let i = 0; i < 3; i++) {
     console.log("Hoera!")
-}*/
+}
 
 
 // ==========================================
@@ -21,13 +21,13 @@
 // klaar!
  // ==========================================
 
-/*for (let i = 0; i < 5; i++) {
+for (let i = 0; i < 5; i++) {
     if (i < 4) {
         console.log("loop");
     } else {
         console.log("klaar!");
     }
-}*/
+}
 
 // ==========================================
 // Opdracht 3. Maak een for-loop die van 0 tot 15 loopt. Voor ieder getal wordt in de console gelogd of het getal even of oneven is.
@@ -39,13 +39,13 @@
 // .... etc.
 // 15 is oneven
 
-/*for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 16; i++) {
     if (i % 2 === 0) {
         console.log(i + " is even");
     } else (
         console.log(i + " is oneven")
     )
-}*/
+}
 
 // Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over..
 // ==========================================
